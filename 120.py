258afd389ff0599b41be0943c0c1217f187ff59a
Claud Gomes <60714768+ClaudHelloworld@users.noreply.github.com>
2020-03-03 22:01:21 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,11 @@
+#Autor: 1DSI3
+   #@Claudinei
+   #Feito_em_03/03/2020
+   #Instituição: ETEC
+
+X = 0 #Declaração_do_Contador
+  valor = 2 #Declaração_da_Variável_Valor
+  while (x <= 21): 
+           valor = valor * valor
+           x = x + 1 #Laço_de_Repetição
+  print (valor) #exibir_a_variável_valor 
